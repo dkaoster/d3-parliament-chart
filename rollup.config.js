cb9e568e013e3f5bd3c52f0989e06207784f3123
Daniel Kao <dkao@diplateevo.com>
2022-04-17 12:35:10 -0700

    Fix D3 global being overwritten

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,17 @@ import babel from '@rollup/plugin-babel';
 
 export default {
   input: './index.js',
-  plugins: [babel({ babelHelpers: 'bundled' })],
+  external: ['d3'],
+  output: {
+    file: 'build/d3-parliament-chart.js',
+    name: 'd3',
+    format: 'umd',
+    indent: false,
+    extend: true,
+    globals: { d3: 'd3' },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**' }),
+    ],
+  },
 };
