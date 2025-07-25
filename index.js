c1fcbcc673f72497440ed5c65da3e233ae744f5e
Daniel Kao <dkao@diplateevo.com>
2025-07-24 21:04:48 -0700

    Fix Build

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 export { default as parliamentChart } from './src/parliament-chart';
-export { default as getParliamentPoints } from './chart-helpers';
+export { default as getParliamentPoints } from './src/chart-helpers';
